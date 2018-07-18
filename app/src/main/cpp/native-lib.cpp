c9fed101d3e9e2cb77df6b8666c6c476696cc175
Maruf Ahmed <maruf.ahmed@northsouth.edu>
2018-07-18 19:40:09 +0600

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,18 @@
+#include <jni.h>
+#include <string>
+#include "opencv2/core.hpp"
+
+
+extern "C" JNIEXPORT jstring
+
+JNICALL
+Java_com_example_batman_newapp_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+
+
+
